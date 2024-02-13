bfb53b79996b3e3b4536bf0674cfa17acf7bb941
Russell Saint Cyr <rcyr@creditshop.com>
2024-02-13 18:34:24 -0500

    Update constants.ts n6qa657mqn

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { KameleoonUtils } from "@kameleoon/javascript-sdk";
 
 // -- Constants values
-export const siteCode = "2p2rgz4sbp";
+export const siteCode = "n6qa657mqn";
 export const featureKey = "bouncing_ball_demo";
 export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
